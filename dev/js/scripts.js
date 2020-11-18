4007cdd50fc8f788796a14e443e21ba147558e1c
emheyy45 <63681829+emheyy45@users.noreply.github.com>
2020-11-18 12:28:22 -0500

    commit_yes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,10 +2,11 @@ import { gsap } from "gsap";
 import { GSDevTools } from "gsap/GSDevTools";
 
 import {flameRotateAnimation} from "./demo.js"
+import {speedAnimation} from "./demo.js"
 
 const mainTL = gsap.timeline();
 mainTL.add(flameRotateAnimation());
-
+mainTL.add(speedAnimation());
 
 
 GSDevTools.create();
